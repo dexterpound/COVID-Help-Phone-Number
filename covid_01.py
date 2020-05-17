70b3f2eb50877beee7cb99ce79fc70fa37f72998
Peter Cockram <dexterpound@gmail.com>
2020-05-17 00:18:13 -0400

    This is the Python file covid_01.py
    This is for the COVID-Help-Phone-Number

---
+++ covid_01.py
@@ -0,0 +1,12 @@
+print ('Do you want to take the Carona Test in North Carolina :Type in "Yes or No"')
+good='Yes'
+aid='phone'
+good = input()
+if (good == 'Yes' or good == 'yes'):
+    print ('The address is 1801 Glendale Dr SW, Wilson, NC 27893. ' +
+    'If you need the Helpline, type "phone" and if not type in "No".')
+    aid = input()
+    if (aid=='phone' or aid == 'Phone'):
+            print("NC CDC Help Line 1-866-462-3821. Don't forget to wash your hands.")
+    else:
+        print('You have elected to do nothing silly human scum, good luck.')
